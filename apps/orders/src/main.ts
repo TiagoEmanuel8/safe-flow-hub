40ae70c0711deadfaaf6c1a7a9bdc457be7285cf
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-01-30 12:15:03 -0300

    feat: implementando base service venda

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
