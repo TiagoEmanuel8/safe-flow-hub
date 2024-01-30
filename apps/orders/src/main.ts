2c77d8ae86c7637dfa58518fd1b597e5c6b06850
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-01-30 11:14:17 -0300

    feat: implementando docker

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 bootstrap();
