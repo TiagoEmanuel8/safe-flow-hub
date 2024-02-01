96cd252bd84566920cdb68c0fd44efa0b554e876
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2024-02-01 15:13:27 -0300

    feat: criando microservices para pedidos

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,10 +1,15 @@
 import { CreateOrderRequest } from './dto/create-order.request';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async createOrder(request: CreateOrderRequest) {
     return this.ordersRepository.create(request);
